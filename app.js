f9be9f0e3e0eab60c99aacfff25702c39b5b335c
nah <2trill2spill@gmail.com>
2016-08-03 16:50:24 -0500

    Add missing lines.

--- app.js
+++ app.js
@@ -5,10 +5,36 @@ $(document).ready(function(){
   var c = new Customer(totalCash);
 
   // Store customer data.
-  $('#container').data("info", c)
+  $('.container').data("info", c)
 
+  $('.container').on('click', ".btn", function() {
 
-  }
+  	console.log("click:")
+
+  	var customerInfo = $('.container').data("info")
+
+  	switch(this.id) {
+  		case 'a':
+  		    customerInfo.apple += 1;
+  		    $('#appleCount').text(customerInfo.apple)
+  		    break;
+  		case 'o':
+  		    customerInfo.oranges += 1;
+  		    $('#orangeCount').text(customerInfo.apple)
+  		    break;
+  		case 'b':
+  		    customerInfo.bananas += 1;
+  		    $('#bananaCount').text(customerInfo.apple)
+  		    break;
+  		case 'p':
+  		    customerInfo.pears += 1;
+  		    $('#pearCount').text(customerInfo.apple)
+  		    break;
+  	}
+
+  	$('.container').data("info", c)
+
+  });
 });
 
 function Customer(cash){
@@ -19,11 +45,12 @@ function Customer(cash){
   this.pears = 0;
 }
 
+// Theresa's Functions ===================================================
 var productPrices = {
   apple: 0,
   orange: 0,
   banana: 0,
-  grapes: 0
+  pear: 0
 };
 
 function priceShift(currentPrice){ // changes the price of a single item
@@ -41,8 +68,6 @@ function priceShift(currentPrice){ // changes the price of a single item
 
   return newPrice;
 }
-
-
 
 function randomNumber(min, max){
   return Math.floor(Math.random() * (1 + max - min) + min);
