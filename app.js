ee7b549fac7abdb5c8a7428603bd15a4dc6a61de
Mat McCully <mccully1894@gmail.com>
2016-08-03 14:12:15 -0500

    some code

---
+++ app.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  // var fruit = ["apples" , "oranges" , "bananas" , "grapes"];
+
+  function Customer(cash){
+    this.cash = cash;
+    this.apple = 0;
+    this.oranges = 0;
+    this.bananas = 0;
+    this.grapes = 0;
+  }
+
+  
+}
